bd4dd79e784daa76fe581d5233e76fc28851931d
Devashish Chakraborty <devashishchakra@gmail.com>
2023-05-28 22:17:56 +0530

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
 import _ from 'lodash';
-import myName from './myName';
-import { functionOne, functionTwo } from './myModule'; 
-import './style.css';
-import Icon from './icon.png'
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js'
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement("button");
   
-    // Lodash, now imported by this script
     element.textContent = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add("hello");
+    
+    btn.textContent = 'Click me and check the console';
+    btn.addEventListener("click", printMe);
 
-    // Add the image to out existing div.
-    const MyIcon  = new Image();
-    MyIcon.src = Icon;
-
-    element.appendChild(MyIcon);
-
-    console.log(Data);
-    console.log(Notes);
+    element.appendChild(btn);
 
     return element;
   }
-  
+
   document.body.appendChild(component());
