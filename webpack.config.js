1a0713186a1886c3517eac708a972b14a9ad8302
Devashish Chakraborty <devashishchakra@gmail.com>
2023-05-28 22:52:44 +0530

    creating source maps and auto code compilations

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
-        print: './src/print.js',    
+        print: './src/print.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        publicPath: '/'
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            title: 'Development',
         }),
     ]
 }
